c450e69d8f183a52b9f8a82b514f40f972227731
elgoode3 <47646593+elgoode3@users.noreply.github.com>
2020-05-03 21:03:07 -0500

    Update Calender.java

--- app/src/main/java/com/example/projecthave2eat/Calender.java
+++ app/src/main/java/com/example/projecthave2eat/Calender.java
@@ -1,14 +1,53 @@
 package com.example.projecthave2eat;
 
+import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.TextView;
+
+import com.example.calendarview.R;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+public class Calendar extends AppCompatActivity {
 
-public class Calender extends AppCompatActivity {
-
+    CalendarView calender;
+    TextView date_view;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_calender);
+        setContentView(R.layout.activity_main);
+
+        calender = (CalendarView)
+                findViewById(R.id.calender);
+        date_view = (TextView)
+                findViewById(R.id.date_view);
+
+
+        calender
+                .setOnDateChangeListener(
+                        new CalendarView
+                                .OnDateChangeListener() {
+                            @Override
+
+
+                            public void onSelectedDayChange(
+                                    @NonNull CalendarView view,
+                                    int year,
+                                    int month,
+                                    int dayOfMonth)
+                            {
+
+
+                                String Date
+                                        = dayOfMonth + "-"
+                                        + (month + 1) + "-" + year;
+
+
+                                date_view.setText(Date);
+                            }
+                        });
     }
 }
+
